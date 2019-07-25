a05e3f40b5057aef9f5a25b83fb9762be5c1e964
Natalia Georgina Lo Duca <natalia.loduca@globant.com>
2019-07-25 13:12:40 -0300

    mejora leer base64 dni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,30 +7,40 @@ import { ResultComponent } from './result/result.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OpenComponent } from './open/open.component';
+import { WebcamModule } from 'ngx-webcam';
 
 import { NgOpenCVModule } from 'ng-open-cv';
+import { DniComponent } from './dni/dni.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { OpenCVOptions } from 'projects/ng-open-cv/src/public_api';
+// import { AppRoutingModule } from './app-routing.module';
+// import { OpenCVOptions } from 'projects/ng-open-cv/src/public_api';
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'result', component: ResultComponent },
+  { path: 'dni', component: DniComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
 ];
-const openCVConfig: OpenCVOptions = {
+/*const openCVConfig: OpenCVOptions = {
   scriptUrl: `assets/opencv/opencv.js`,
   wasmBinaryFile: 'wasm/opencv_js.wasm',
   usingWasm: true
-};
+};*/
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ResultComponent, OpenComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ResultComponent,
+    OpenComponent,
+    DniComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    NgOpenCVModule.forRoot(openCVConfig),
-    AppRoutingModule
+    WebcamModule
+    /*NgOpenCVModule.forRoot(openCVConfig),
+    AppRoutingModule*/
   ],
   providers: [],
   bootstrap: [AppComponent]
