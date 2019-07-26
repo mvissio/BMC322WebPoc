7e11b442931fd1f83cbd92d3b6fea6d1d5416258
marcos vissio <marcos.vissio@globant.com>
2019-07-25 23:12:15 -0300

    Poc Biometrica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ResultComponent } from './result/result.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { OpenComponent } from './open/open.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ResultComponent} from './result/result.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {OpenComponent} from './open/open.component';
 
-import { NgOpenCVModule } from 'ng-open-cv';
+import {NgOpenCVModule, OpenCVOptions} from 'ng-open-cv';
 
-import { AppRoutingModule } from './app-routing.module';
-import { OpenCVOptions } from 'projects/ng-open-cv/src/public_api';
+// import { AppRoutingModule } from './app-routing.module';
+import {FaceApiComponent} from './face-api/face-api.component';
+import {FormsModule} from '@angular/forms';
+
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'result', component: ResultComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: HomeComponent }
+  {path: 'home', component: HomeComponent},
+  {path: 'open', component: OpenComponent},
+  {path: 'result', component: ResultComponent},
+  {path: 'faceapi', component: FaceApiComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent}
 ];
 const openCVConfig: OpenCVOptions = {
   scriptUrl: `assets/opencv/opencv.js`,
   wasmBinaryFile: 'wasm/opencv_js.wasm',
   usingWasm: true
 };
+
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ResultComponent, OpenComponent],
+  declarations: [AppComponent, HomeComponent, ResultComponent, OpenComponent, FaceApiComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
     NgOpenCVModule.forRoot(openCVConfig),
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
